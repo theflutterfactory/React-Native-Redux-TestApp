a8dddd74c25ab7f9c16224831e0ad22318a06fa5
Julian Currie <julian.currie@gmail.com>
2016-11-08 02:30:33 -0800

    added a react-redux Provider and central reducer file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-
 import {
   View
 } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 export default class Main extends Component {
   render() {
     return (
-      <View />
-    );
+      <Provider store={createStore(reducers)}>
+        <View />
+      </Provider>
+        );
   }
 }
