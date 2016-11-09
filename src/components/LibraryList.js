e8f2171d8c5e08bfcde3bf1437b8b4c34fbb029a
Julian Currie <julian.currie@gmail.com>
2016-11-08 20:40:06 -0800

    added a ListView component with separator

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import {
+  ListView,
+  connect,
+  View,
+  StyleSheet
+} from 'react-redux';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow() {
+
+  }
+
+  renderSeparator(sectionID, rowID) {
+    return (
+      <View style={styles.separator} key={sectionID + rowID} />
+    );
+  }
+
   render() {
-    return;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+        renderSeparator={this.renderSeparator} />
+    );
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
+  return { libraries: state.libraries };
 };
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    backgroundColor: '#90141414'
+  }
+});
 
 export default connect(mapStateToProps)(LibraryList);
